04a299965141c18fecb06f1929fa476466777eaa
Michael Francis <michael@melenion.com>
2022-07-15 15:56:45 -0300

    a lil cleanup

--- build.rs
+++ build.rs
@@ -1,20 +1,19 @@
 use proc_macro2::TokenStream;
 use serde::Deserialize;
-use std::fmt::format;
 use std::fs;
 use std::path::Path;
 use std::{env, io::BufRead, path::PathBuf, process::Command};
 
 #[derive(Debug, Deserialize)]
-struct Target {
-    name: String,
-    test: bool,
+pub struct Target {
+    pub name: String,
+    pub test: bool,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
 
-enum Message {
+pub enum Message {
     CompilerArtifact { executable: PathBuf, target: Target },
     Other { reason: String },
 }
@@ -54,24 +53,16 @@ fn get_test_paths(input: PathBuf) -> Result<Vec<PathBuf>, ()> {
 
 fn main() {
     let input = fs::canonicalize(env::var("INPUT_DIR").unwrap()).unwrap();
-    let output = env::var("OUT_DIR").unwrap();
-    println!("output: {output}");
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("tests.rs");
     let paths = get_test_paths(input).unwrap();
     let path_strs = paths
         .iter()
         .map(|s| format!(r#"{}"#, s.to_str().unwrap()))
-        .collect::<Vec<String>>();
-    let printme = format!("{}", path_strs.join(","));
+        .collect::<Vec<String>>()
+        .join(",");
 
-    let out: Vec<TokenStream> = paths
-        .iter()
-        .map(|f| {
-            let x = f.to_str().unwrap();
-            quote::quote! { #x }
-        })
-        .collect();
+    let printme = format!("{}", path_strs);
 
     let out = quote::quote! {
         use std::{env, io::BufRead, path::PathBuf, process::Command};
@@ -85,7 +76,6 @@ fn main() {
     };
 
     fs::write(&dest_path, TokenStream::from(out).to_string()).unwrap();
-    // println!("cargo:rerun-if-changed=build.rs");
     // fs::create_dir(&output).expect("couldn't create dir");
     // panic!("just because");
 }
